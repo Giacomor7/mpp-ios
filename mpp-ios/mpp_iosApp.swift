2c57bf416dab52065afc43b40487d370fec749ab
giaros <giacomor7@gmail.com>
2024-07-03 17:01:52 +0100

    Initial Commit

---
+++ mpp-ios/mpp_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mpp_iosApp.swift
+//  mpp-ios
+//
+//  Created by Giacomo Rossetti on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct mpp_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
