dc7be914ed15272cd7bffead13e68fbbcba23c41
Kenny Cho <Kenny.Cho@softwire.com>
2024-07-04 14:03:39 +0100

    Add picker functionality and placeholder button

--- mpp-ios/ContentView.swift
+++ mpp-ios/ContentView.swift
@@ -7,13 +7,52 @@
 
 import SwiftUI
 
+struct Station: Identifiable, Hashable {
+    let name: String
+    let id: String
+}
+
 struct ContentView: View {
+    
+    var stations = [
+        Station(name: "London Kings Cross", id: "KGX"),
+        Station(name: "Edinburgh Waverley", id: "EDB"),
+        Station(name: "Newcastle", id: "NCL"),
+        Station(name: "York", id: "YRK"),
+        Station(name: "Leeds", id: "LDS")
+    ]
+    @State private var departStation: Station? = nil
+    @State private var arrivalStation: Station? = nil
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Live Train Times")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.all)
+            
+            HStack {
+                Picker("Select departure station", selection: $departStation) {
+                    Text("Select departure...").tag(Optional<Station>(nil))
+                    ForEach(stations) { station in
+                        Text(station.name).tag(station as Station?)
+                    }
+                }
+                .frame(width: 150)
+                Picker("Select arrival station", selection: $arrivalStation) {
+                    Text("Select arrival...").tag(Optional<Station>(nil))
+                    ForEach(stations) { station in
+                        Text(station.name).tag(station as Station?)
+                    }
+                }
+                .frame(width: 150)
+            }
+            Button("Submit") {
+                
+            }
+            .padding()
+            .font(.title3)
+            Spacer()
         }
         .padding()
     }
